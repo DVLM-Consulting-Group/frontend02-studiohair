b797e842cbb28e12e27bd7d1690d272a3e18548f
matheuscaso <matheuscasoares@gmail.com>
2022-09-13 15:33:42 -0300

    HTML e CSS para Lista de Funcionários

--- src/app/components/funcionario/funcionario-list/funcionario-list.component.ts
+++ src/app/components/funcionario/funcionario-list/funcionario-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Funcionario } from 'src/app/models/funcionario';
 
 @Component({
   selector: 'app-funcionario-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FuncionarioListComponent implements OnInit {
 
+  ELEMENT_DATA: Funcionario[] = [
+{
+    id: 1,
+    nome: 'Matheus Cavalcante',
+    cpf: '123.456.789-10',
+    email: 'matheus@gmail.com',
+    senha: '123456789',
+    perfis: ['0'],
+    dataCriacao: '15/10/2022'
+  }
+  ]
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Funcionario>(this.ELEMENT_DATA);
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
---
+++ src/app/models/funcionario.ts
@@ -0,0 +1,10 @@
+export interface Funcionario {
+
+    id?: any;
+    nome: String;
+    cpf: String;
+    email: String;
+    senha: String;
+    perfis: String[];
+    dataCriacao: any;
+}

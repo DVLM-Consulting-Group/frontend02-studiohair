338cd88019bd24d8b4257c7ed9d6a04c48cbf53e
matheuscaso <matheuscasoares@gmail.com>
2022-09-13 12:39:47 -0300

    Rota para Funcioario-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FuncionarioListComponent } from './components/funcionario/funcionario-list/funcionario-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent , children: [
-      {path: 'home', component: HomeComponent}
+      {path: 'home', component: HomeComponent},
+      {path: 'funcionarios', component: FuncionarioListComponent}
     ]
   },
 

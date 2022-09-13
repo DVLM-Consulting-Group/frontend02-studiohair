d0161570e08bfa25eadaa5891fa5ade7d0b55735
matheuscaso <matheuscasoares@gmail.com>
2022-09-13 18:03:06 -0300

    validação de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Login } from 'src/app/models/login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  log: Login = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean{
+    if(this.email.valid && this.senha.valid){
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }

2a5e48fb0561dcff2f0817c3c98e20b5103032a4
matheuscaso <matheuscasoares@gmail.com>
2022-09-13 20:50:58 -0300

    autenticação de login

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Login } from '../models/login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  jwtService: JwtHelperService = new JwtHelperService();
+
+  constructor(private http: HttpClient) { }
+
+  authenticate (log: Login) {
+    return this.http.post('${API_CONFIG.baseUrl}/login' , log, {
+      observe: 'response',
+      responseType: 'text'
+    })
+  }
+  sucessfulLogin(authToken: string) {
+    localStorage.setItem('token', authToken);
+  }
+
+  isAuthenticated() {
+    let token = localStorage.getItem('token')
+    if(token != null) {
+      return !this.jwtService.isTokenExpired(token)
+    }
+    return false;
+  }
+}
